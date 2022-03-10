4d0e3bf53b8d061f3a48369af7823af5f01e078d
jsfdz <crisanfdez@gmail.com>
2022-03-10 00:19:54 -0600

    first commit

---
+++ main.js
@@ -0,0 +1,86 @@
+const signsZodiac = [
+  {
+    id: 1,
+    sign: 'Aries',
+    image: 'img/1-aries.jpg',
+    date: 'March 21 - April 19',
+    description: 'Aries is the first sign of the zodiac, and is the most active and energetic.'
+  },
+  {
+    id: 2,
+    sign: 'Taurus',
+    image: 'img/2-taurus.jpg',
+    date: 'April 20 - May 20',
+    description: 'Taurus is the second sign of the zodiac, and is the most conservative and traditional.'
+  },
+  {
+    id: 3,
+    sign: 'Gemini',
+    image: 'img/3-gemini.jpg',
+    date: 'May 21 - June 20',
+    description: 'Gemini is the third sign of the zodiac, and is the most intellectual and creative.'
+  },
+  {
+    id: 4,
+    sign: 'Cancer',
+    image: 'img/4-cancer.jpg',
+    date: 'June 21 - July 22',
+    description: 'Cancer is the fourth sign of the zodiac, and is the most emotional and sensitive.'
+  },
+  {
+    id: 5,
+    sign: 'Leo',
+    image: 'img/5-leo.jpg',
+    date: 'July 23 - August 22',
+    description: 'Leo is the fifth sign of the zodiac, and is the most self-confident and dominant.'
+  },
+  {
+    id: 6,
+    sign: 'Virgo',
+    image: 'img/6-virgo.jpg',
+    date: 'August 23 - September 22',
+    description: 'Virgo is the sixth sign of the zodiac, and is the most practical.'
+  },
+  {
+    id: 7,
+    sign: 'Libra',
+    image: 'img/7-libra.jpg',
+    date: 'September 23 - October 22',
+    description: 'Libra is the seventh sign of the zodiac, and is the most balanced.'
+  },
+  {
+    id: 8,
+    sign: 'Scorpio',
+    image: 'img/8-scorpio.jpg',
+    date: 'October 23 - November 21',
+    description: 'Scorpio is the eighth sign of the zodiac, and is the most passionate.'
+  },
+  {
+    id: 9,
+    sign: 'Sagittarius',
+    image: 'img/9-sagittarius.jpg',
+    date: 'November 22 - December 21',
+    description: 'Sagittarius is the ninth sign of the zodiac, and is the most adventurous.'
+  },
+  {
+    id: 10,
+    sign: 'Capricorn',
+    image: 'img/10-capricorn.jpg',
+    date: 'December 22 - January 19',
+    description: 'Capricorn is the tenth sign of the zodiac, and is the most disciplined.'
+  },
+  {
+    id: 11,
+    sign: 'Aquarius',
+    image: 'img/11-aquarius.jpg',
+    date: 'January 20 - February 18',
+    description: 'Aquarius is the eleventh sign of the zodiac, and is the most humanitarian.'
+  },
+  {
+    id: 12,
+    sign: 'Pisces',
+    image: 'img/12-pisces.jpg',
+    date: 'February 19 - March 20',
+    description: 'Pisces is the twelfth sign of the zodiac, and is the most introverted.'
+  }
+]
